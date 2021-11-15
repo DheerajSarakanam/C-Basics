677974d70f2c2e7f54b4fe7e26805f12f891153e
DheerajSarakanam <92290772+DheerajSarakanam@users.noreply.github.com>
2021-11-15 12:36:05 +0530

    Add files via upload

---
+++ input and output 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c, p;
+	printf("Enter three numbers");
+	scanf("%d%d%d", &a, &b, &c);
+	p = a*b*c;
+	printf("product of %d, %d, %d is %d", a, b, c, p);	
+}
+
+
+
+
+
+
---
+++ input and output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+	int n;
+	printf("Enter a number to get the square of that number : ");
+	scanf("%d", &n);
+	printf("square is : %d", n*n);
+}
+
