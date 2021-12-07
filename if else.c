f61c909df02fd939d5f80c8900ca6f277f1e6390
DheerajSarakanam <92290772+DheerajSarakanam@users.noreply.github.com>
2021-12-07 12:28:21 +0530

    Add files via upload

---
+++ if else.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("to check a day");
+	printf(	"\nenter a number"); 
+	scanf("%d",&n);
+	if(n=1)
+	{
+		printf("%d is sunday",n);	
+	}
+	else if(n=2)
+	{
+		printf("%d is monday ",n);
+	}
+	else 
+	printf("%d is tuesday",n);
+}
+	
+
